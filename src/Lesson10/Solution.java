d5ceb39434e208cbf118eaa7ed3ddc2505a7b369
Richard <seter74@gmail.com>
2024-11-08 23:21:10 +0100

    homework 9

--- src/Lesson10/Solution.java
+++ src/Lesson10/Solution.java
@@ -1,4 +1,7 @@
 package Lesson10;
+
+import java.util.Arrays;
+
 //        1. Find and return the most common letter in a given string.
 //        2. Check if 2 strings are anagrams of each other (below - elbow, end - ned, apple - ppale)
 //        3. Compress a string using the following rule: aaabccc = a3bc3
@@ -7,6 +10,9 @@ public class Solution {
     public static void main(String[] args) {
         char mostCommonLetter = getMostCommonLetter("What is the most common letter here?");
         System.out.println(mostCommonLetter);
+
+        boolean isAnagram = isAnagram("apple", "ppale");
+        System.out.println(isAnagram);
     }
 
     //This method returns the letter that is most frequently in the given string. If several letters occur equally often,
@@ -38,5 +44,20 @@ public class Solution {
             }
         }
         return result;
+    }
+
+    static boolean isAnagram(String str1, String str2) {
+        if ((str1 == null || str1.isEmpty()) || (str2 == null || str2.isEmpty())) {
+            return false;
+        }
+        if (str1.equalsIgnoreCase(str2)) {
+            return true;
+        }
+        char[] a = str1.toLowerCase().toCharArray();
+        char[] b = str2.toLowerCase().toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
     }
 }
