4969a26d33ec41fbdcc6c122965b279de7ace345
Richard <seter74@gmail.com>
2024-11-09 22:16:28 +0100

    homework 10 5th commit, include binary converter

--- src/Lesson10/Converter.java
+++ src/Lesson10/Converter.java
@@ -5,18 +5,23 @@ public class Converter {
     public final static char[] values = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     public static void main(String[] args) {
-        System.out.println(toBinary(4513));
+        System.out.println(intToBinaryString(12201458));
+        System.out.println(Integer.toBinaryString(12201458)+ "\n");//Test output for comparing the result
+
     }
 
-    static String toBinary(int value) {
+    static String intToBinaryString(int value) {
         StringBuilder sb = new StringBuilder();
-
         while (value > 0) {
             sb.append(value%2);
             value /= 2;
         }
         return sb.reverse().toString();
     }
+
+//    static String hexToBinaryString(String value) {
+//
+//    }
 
     static long power(int base, int exponent) {
         int result = 1;
