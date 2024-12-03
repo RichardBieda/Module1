f4a9a3a0b874e8bf592ff97991c4adfe127e13be
Richard <seter74@gmail.com>
2024-12-03 13:00:23 +0100

    homework 14 MyArray class

---
+++ src/Lesson14/Main.java
@@ -0,0 +1,24 @@
+package Lesson14;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        MyArray myArray = new MyArray("1", "2");
+
+        System.out.println(myArray.getSize() + "   " + myArray);
+        myArray.add("3");
+        myArray.add("4");
+        myArray.add("5");
+        myArray.add("6");
+        myArray.add("7");
+        myArray.add("8");
+        myArray.add("9");
+        myArray.add("10");
+        myArray.add("11");
+        myArray.add("12");
+        System.out.println(myArray.getSize() + "   " + myArray);
+        myArray.trimToSize();
+        System.out.println(myArray.getSize() + "   " + myArray);
+    }
+}
---
+++ src/Lesson14/MyArray.java
@@ -0,0 +1,73 @@
+package Lesson14;
+
+import java.util.Arrays;
+
+public class MyArray {
+
+    private int startLength = 10;
+    private String[] elements = new String[startLength];
+    private int size;
+
+    public MyArray() {
+    }
+
+    public MyArray(String...elements) {
+        for (String s : elements) {
+            add(s);
+        }
+    }
+
+    public void add(String element) {
+        if (size < elements.length) {
+            elements[size++] = element;
+        } else {
+            size();
+            elements[size++] = element;
+        }
+    }
+
+    public String get(int index) {
+        if (index <= size) {
+            return elements[index];
+        } else {
+            System.out.println("index is out of bounds");
+        }
+        return null;
+    }
+
+    public void size() {
+        int growingSize = elements.length + (elements.length / 2) + 1;
+        String[] newArray = new String[growingSize];
+        for (int i = 0; i < elements.length; i++) {
+            newArray[i] = elements[i];
+        }
+        elements = newArray;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void trimToSize() {
+        String[] newArray = new String[size];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = elements[i];
+        }
+        elements = newArray;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("[");
+        for (int i = 0; i < size; i++) {
+            if (i < size-1) {
+                result.append(elements[i] + ",");
+            } else {
+                result.append(elements[i]);
+            }
+        }
+        result.append("]");
+        return result.toString();
+    }
+}
