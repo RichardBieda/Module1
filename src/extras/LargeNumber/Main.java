1a09001859b0d2d50debca3a2064532f73186f94
Richard <seter74@gmail.com>
2024-12-08 19:38:21 +0100

    LargeNumberClass 3 commit

--- src/extras/LargeNumber/Main.java
+++ src/extras/LargeNumber/Main.java
@@ -6,9 +6,13 @@ public class Main {
 
     public static void main(String[] args) throws LargeNumberValueException {
 
-        LargeNumber con = new LargeNumber("1234567891122334455667788995427845425157674845275194979445652762941576546459542652621975164516725649159167595495", 10);
-        con.divide("5427845425167889542512455");
-        System.out.println(con.getValue());
+        LargeNumber l1 = new LargeNumber("f7a4", 16);
+        l1.divide("b2");
+        System.out.println(l1.getValue()); //164
+
+        LargeNumber l2 = new LargeNumber("1234567891122334455667788995427845425157674845275194979445652762941576546459542652621975164516725649159167595495", 10);
+        l2.divide("5427845425167889542512455");
+        System.out.println(l2.getValue());
         BigInteger big1 = new BigInteger("1234567891122334455667788995427845425157674845275194979445652762941576546459542652621975164516725649159167595495");
         BigInteger big2 = new BigInteger("5427845425167889542512455");
         System.out.println(big1.divide(big2));
