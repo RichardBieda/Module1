23d12bfb3f3675d9ff5897799092814f59cfb7e9
Richard <seter74@gmail.com>
2024-12-16 10:12:18 +0100

    MyArrayList fourth commit included remove and set

--- src/Lesson15/Main.java
+++ src/Lesson15/Main.java
@@ -25,5 +25,7 @@ public class Main {
         System.out.println(cl);
         cl.set(0, 'Z');
         System.out.println(cl + " " + cl.size());
+        cl.remove(cl.size()-1);
+        System.out.println(cl);
     }
 }
