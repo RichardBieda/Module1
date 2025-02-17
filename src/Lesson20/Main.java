da0740b2267694149e5c764c875d480df03c7f3f
Richard <seter74@gmail.com>
2025-02-17 17:17:04 +0100

    spellchecker third commit some bugfixes plus dict

--- src/Lesson20/Main.java
+++ src/Lesson20/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         //insert your path
-        AutoCorrector autoCorrector = new AutoCorrector("C:\\Users\\Richard\\Module1\\src\\Dictionary_AutoCorrector");
+        AutoCorrector autoCorrector = new AutoCorrector("C:\\Users\\Richard\\Module1\\src\\ENGLISH_DICT");
     }
 }
