9462fa81b51e77c896f426d0658e26154edf06a0
Richard <seter74@gmail.com>
2025-02-19 20:18:36 +0100

    spellchecker fourth commit upgraded search time to nanoseconds. inserting of the ability to show the count of loaded and added words. adjustment of editing distance.

--- src/Lesson20/BKTree.java
+++ src/Lesson20/BKTree.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
 
 //This is an implementation of a Burkhard Keller Tree, the first Node to be added is the root
 //Nodes have a String word field
 //Nodes have a Map of children, this map stores the edit-distance between root and child as key and the childNode itself as value
 //Every childMap stores only one Node per key, if (edit-distance thus key) of a second Node is equal, then it become a child of the child.
-//to change the search options go to line 102
+//line 116 is important for the search results. Decreasing the range means to get faster results, but you will lose precision
 class BKTree {
 
     private final static int STANDARD_TOLERANCE = 2;
@@ -26,19 +28,23 @@ class BKTree {
 
     private int suggests = 0;
 
-    private long time = 0;
+    private int wordCount = 0;
+
+    private Duration time;
 
     BKTree(String dictionary) {
         this.dictionary = dictionary;
         this.newWords = new HashSet<>();
         this.tolerance = STANDARD_TOLERANCE;
         loadDictionary();
+        System.out.println(wordCount + " Strings added to dictionary");
     }
     BKTree(String dictionary, int tolerance) {
         this.dictionary = dictionary;
         this.newWords = new HashSet<>();
         this.tolerance = tolerance;
         loadDictionary();
+        System.out.println(wordCount + " Strings added to dictionary");
     }
 
     boolean add(String input) {
@@ -48,23 +54,26 @@ class BKTree {
         String word = input.toLowerCase();
         if (root == null) {
             root = new Node(word);
+            wordCount++;
             return true;
         }
         Node tmpNode = root;
         while (true) {
-            int distance = EditingDistance.distance(word, tmpNode.word);
+            int distance = EditingDistance.distance2(word, tmpNode.word);
             if (distance == 0) {
                 return false;
             }
             if (tmpNode.children == null) {
                 tmpNode.children = new HashMap<>();
                 tmpNode.children.put(distance, new Node(word));
+                wordCount++;
                 return true;
             } else {
                 if (tmpNode.children.containsKey(distance)) {
                     tmpNode = tmpNode.children.get(distance);
                 } else {
                     tmpNode.children.put(distance, new Node(word));
+                    wordCount++;
                     return true;
                 }
             }
@@ -77,17 +86,17 @@ class BKTree {
         if (root == null) return results;
         Stack<Node> nodeStack = new Stack<>();
         nodeStack.push(root);
-        long startTime = new Date().getTime();
+        Instant start = Instant.now();
         searchWordsThroughLevels(results, nodeStack, word);
-        long endTime = new Date().getTime();
-        time = endTime - startTime;
+        Instant end = Instant.now();
+        time = Duration.between(start, end);
         return results;
     }
 
     private void searchWordsThroughLevels(Map<Integer, List<String>> map, Stack<Node> stack, String word) {
         while (!stack.isEmpty()) {
             Node tmpNode = stack.pop();
-            int distance = EditingDistance.distance(word, tmpNode.word);
+            int distance = EditingDistance.distance2(word, tmpNode.word);
             queries++;
             if (distance <= tolerance && distance > 0) {
                 if (map.containsKey(distance)) {
@@ -120,6 +129,7 @@ class BKTree {
             for (String x : list) {
                 add(x.trim());
             }
+            System.out.println(list.size() + " Strings loaded from file");
         } catch (IOException e) {}
     }
 
@@ -136,13 +146,13 @@ class BKTree {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        newWords.clear();
     }
 
     void getTreeInfos() {
-        System.out.println(suggests + " suggests by " + queries + " queries in " + time + " milliseconds");
+        System.out.println(suggests + " suggests by " + queries + " queries in " + time.getSeconds() + "." + time.getNano() + " seconds");
         queries = 0;
         suggests = 0;
-        time = 0;
     }
 
     private class Node {
--- src/Lesson20/Main.java
+++ src/Lesson20/Main.java
@@ -1,10 +1,20 @@
 package Lesson20;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         //insert your path
+        //end your word with a semicolon to save the word in the tree
+        //write end; to close the programm and save the new words in file
+        //the tolerance is the number of permissible wrongs in your word, 2 is the standard tolerance
+        //There are 2 constructors
+        //new AutoCorrector(Path path); and new AutoCorrector(Path path, int tolerance);
         AutoCorrector autoCorrector = new AutoCorrector("C:\\Users\\Richard\\Module1\\src\\ENGLISH_DICT");
     }
 }
