e372bf83cb2e0287db56f5cc7195d7adccfcc4a0
Richard <seter74@gmail.com>
2025-03-02 20:08:16 +0100

    9th commit

--- src/Module1_Final_Project/CryptoMessenger.java
+++ src/Module1_Final_Project/CryptoMessenger.java
@@ -6,12 +6,9 @@ public class CryptoMessenger {
 
     private final CaesarCipher caesarCipher;
     private final FileManager fileManager;
-    private final String USER_NAME;
-
 
     public CryptoMessenger(String userName, int key) {
         this.caesarCipher = new CaesarCipher(key);
         this.fileManager = new FileManager(userName);
-        this.USER_NAME = userName;
     }
 }
