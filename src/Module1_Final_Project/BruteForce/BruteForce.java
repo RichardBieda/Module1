f006270192f8098962f6ed5560803180c0f5cd04
Richard <seter74@gmail.com>
2025-03-09 21:23:49 +0100

    17th commit with bruteforce and node and dictionary

--- src/Module1_Final_Project/BruteForce/BruteForce.java
+++ src/Module1_Final_Project/BruteForce/BruteForce.java
@@ -2,7 +2,11 @@ package Module1_Final_Project.BruteForce;
 
 final class BruteForce {
 
+    private static final char[] ALPHA = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','m','x','y','z',' ','!','?',',','.','\'',':','0','1','2','3','4','5','6','7','8','9'};
+    private static final int LENGTH = ALPHA.length;
     private final Dictionary DICTIONARY;
+
+
     private int range;
 
     BruteForce(String dictionaryPath, int range) {
--- src/Module1_Final_Project/BruteForce/Dictionary.java
+++ src/Module1_Final_Project/BruteForce/Dictionary.java
@@ -9,7 +9,7 @@ class Dictionary {
 
     private final static Set<Character> CRYPTO_ALPHABET = new HashSet();
     static {
-        Collections.addAll(CRYPTO_ALPHABET, 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','!','?',',','.','\'',':',';','-','+','*','/','%','(',')','0','1','2','3','4','5','6','7','8','9');
+        Collections.addAll(CRYPTO_ALPHABET, 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','m','x','y','z',' ','!','?',',','.','\'',':','0','1','2','3','4','5','6','7','8','9');
     }
     private final BFNode root;
     private final String DICTIONARY_PATH;
