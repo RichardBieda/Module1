628de229718e7b16d36e629547acb7829ac37628
Richard <seter74@gmail.com>
2025-04-11 01:54:31 +0200

    24th commit wildcard methods

--- src/Maze/Main.java
+++ src/Maze/Main.java
@@ -14,9 +14,6 @@ public class Main {
 
         Maze m = new Maze(16, 16);
 
-        m.setDestination(0, 0);
-        m.setStart(10, 15);
-
         m.setVerticalField(1, 8, 6, Wall.class);
         m.setVerticalField(8, 8, 8, Wall.class);
 //
@@ -28,28 +25,22 @@ public class Main {
         m.setVerticalField(1, 10, 1, Bridge.class);
         m.setVerticalField(7, 4, 4, NoFlyZone.class);
 
-        Movable user = new Amphibious();
+        m.setDestination(10, 2);
+        m.setStart(0, 15);
+
+        DisplayConsole dis = new DisplayConsole();
+        System.out.println(dis.display(m));
+
+        Movable user = new Pedestrian();
         MazeSolver solver = new MazeSolver(user);
+
         try {
             solver.solve(m);
         } catch (PathNotFoundException | NullPointerException e) {
             System.out.println(e);
         }
-        DisplayConsole dis = new DisplayConsole();
+
         System.out.println(dis.display(m));
         System.out.println("steps count: " + solver.getSteps());
-
-        m.setDestination(10, 2);
-        m.setStart(0, 15);
-
-        Movable user1 = new Pedestrian();
-        MazeSolver solver1 = new MazeSolver(user1);
-        try {
-            solver1.solve(m);
-        } catch (PathNotFoundException | NullPointerException e) {
-            System.out.println(e);
-        }
-        System.out.println(dis.display(m));
-        System.out.println("steps count: " + solver1.getSteps());
     }
 }
