3d49f33b1a0397c90f2b12061130b80f48f2e745
Richard <seter74@gmail.com>
2025-06-02 02:22:37 +0200

    Maze 14th commit pngTest

---
+++ src/Maze/Files/PNGTest.java
@@ -0,0 +1,34 @@
+package Maze.Files;
+
+import javax.swing.*;
+import java.awt.*;
+
+class PNGTest extends JFrame {
+
+    static Image icon = new ImageIcon("src\\Maze\\Files\\NO_FLY_ZONE.png").getImage();
+
+    PNGTest() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(new APanel());
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(PNGTest::new);
+     }
+
+     class APanel extends JPanel {
+
+        APanel() {
+            setPreferredSize(new Dimension(400, 400));
+            setLayout(new GridBagLayout());
+        }
+
+        @Override
+         protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(icon, 0, 0, getWidth(), getHeight(), this);
+        }
+     }
+}
